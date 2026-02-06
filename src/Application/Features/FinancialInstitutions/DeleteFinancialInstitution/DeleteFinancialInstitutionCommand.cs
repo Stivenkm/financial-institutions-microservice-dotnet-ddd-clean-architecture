845fa1e97644de9f0fb03ca99a794b9dd4d368a8
STiven González Anzola <stivenkamilo@gmail.com>
2026-02-06 16:21:38 -0500

    feat(application): add DeleteFinancialInstitution command and endpoint (#5)

---
+++ src/Application/Features/FinancialInstitutions/DeleteFinancialInstitution/DeleteFinancialInstitutionCommand.cs
@@ -0,0 +1,8 @@
+﻿using Intec.Banking.FinancialInstitutions.Domain.ValueObjects;
+using Intec.Banking.FinancialInstitutions.Primitives;
+
+namespace Intec.Banking.FinancialInstitutions.Application.Features.FinancialInstitutions.DeleteFinancialInstitution;
+public class DeleteFinancialInstitutionCommand : ICommand<FinancialInstitutionId>
+{
+    public FinancialInstitutionId Id { get; set; } = default!;
+}
---
+++ src/Application/Features/FinancialInstitutions/DeleteFinancialInstitution/DeleteFinancialInstitutionValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Intec.Banking.FinancialInstitutions.Application.Features.FinancialInstitutions.DeleteFinancialInstitution;
+
+public class DeleteFinancialInstitutionValidator : AbstractValidator<DeleteFinancialInstitutionCommand>
+{
+    public DeleteFinancialInstitutionValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Financial institution id is required");
+    }
+}
